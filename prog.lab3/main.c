29b294be1f3024f55eafa210eecf3ab292b261be
lerchonok <lera.demina.12@mail.ru>
2021-12-13 18:45:40 +0300

    change file

--- prog.lab3/main.c
+++ prog.lab3/main.c
@@ -5,11 +5,11 @@ int main()
 {
     unsigned int znach, znach2;
     scanf("%o", &znach);
-    printf("%d\n", znach);
-    printf("%o\n", znach >> 3);
-    printf("%o\n", ~znach);
+    printf("Number %d\n", znach);
+    printf("Number %o\n", znach >> 3);
+    printf("Number %o\n", ~znach);
 
     scanf("%o", &znach2);
-    printf("%o", znach | znach2);
+    printf("Number %o", znach | znach2);
     return 0;
 }
